36f32f0fa76382c55543fa8ce0a139aedd4564e3
NataliNascimento <natali_nascimento@outlook.com>
2023-04-11 10:43:46 -0300

    Create OpenCalculator.py

---
+++ OpenCalculator.py
@@ -0,0 +1,27 @@
+import pyautogui as posicaoMouse
+import pyautogui as tempoEspera
+
+#wait computer response.
+tempoEspera.sleep(1)
+
+#move mouse to the Window button, it's not necessary to move the mouse button because the click function automatically move the mouse button.
+#posicaoMouse.moveTo(680, 1058)
+
+#wait computer response.
+#tempoEspera.sleep(1)
+
+#clicking windows button. This position changes according your computer.
+#To get the position of your Windows button use print(posicaoMouse.position()) command.
+posicaoMouse.click(680, 1058)
+
+#wait computer response.
+tempoEspera.sleep(1)
+
+#writing application name, in this case calculator.
+posicaoMouse.typewrite('calculadora')
+
+#wait computer response.
+tempoEspera.sleep(2)
+
+#clicking calculator application.
+posicaoMouse.click(696, 481)
