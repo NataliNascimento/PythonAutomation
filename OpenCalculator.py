9e7d7456e527c77aeeb604a801d0c71c70aa076c
NataliNascimento <natali_nascimento@outlook.com>
2023-04-12 19:57:25 -0300

    Update OpenCalculator.py

--- OpenCalculator.py
+++ OpenCalculator.py
@@ -1,30 +1,30 @@
-#this project will open calulator.
-#the positions of click will change according your window distribuition.
+# this project will open calulator.
+# the positions of click will change according your window distribuition.
 
 import pyautogui as posicaoMouse
 import pyautogui as tempoEspera
 
-#wait computer response.
+# wait computer response.
 tempoEspera.sleep(1)
 
-#move mouse to the Window button, it's not necessary to move the mouse button because the click function automatically move the mouse button.
-#posicaoMouse.moveTo(680, 1058)
+# move mouse to the Window button, it's not necessary to move the mouse button because the click function automatically move the mouse button.
+# posicaoMouse.moveTo(680, 1058)
 
-#wait computer response.
-#tempoEspera.sleep(1)
+# wait computer response.
+# tempoEspera.sleep(1)
 
-#clicking windows button. This position changes according your computer.
-#To get the position of your Windows button use print(posicaoMouse.position()) command.
+# clicking windows button. This position changes according your computer.
+# To get the position of your Windows button use print(posicaoMouse.position()) command.
 posicaoMouse.click(680, 1058)
 
-#wait computer response.
+# wait computer response.
 tempoEspera.sleep(1)
 
-#writing application name, in this case calculator.
+# writing application name, in this case calculator.
 posicaoMouse.typewrite('calculadora')
 
-#wait computer response.
+# wait computer response.
 tempoEspera.sleep(2)
 
-#clicking calculator application.
+# clicking calculator application.
 posicaoMouse.click(696, 481)
