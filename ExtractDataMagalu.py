72f9f0c1237ea9db0c597511a1110e26b1ee237a
NataliNascimento <natali_nascimento@outlook.com>
2023-04-15 17:12:03 -0300

    Create ExtractDataMagalu.py

---
+++ ExtractDataMagalu.py
@@ -0,0 +1,42 @@
+from selenium import webdriver as seleniumOptions
+from selenium.webdriver.common.by import By
+import pyautogui as timeResponse
+import pyautogui as keyboardFunctions
+
+# get browser that will use.
+browser = seleniumOptions.Chrome()
+
+# open url to get the data
+browser.get('https://www.magazineluiza.com.br/')
+
+# find id from label search and type the name of the product.
+browser.find_element(By.ID, 'input-search').send_keys('geladeira')
+
+# wait computer response.
+timeResponse.sleep(2)
+
+# send hotkey enter.
+keyboardFunctions.press('enter')
+
+# wait computer response.
+timeResponse.sleep(4)
+
+productList = browser.find_elements(By.CLASS_NAME, 'fwviCj')
+
+for item in productList:
+    productName = ''
+    produtPrice = ''
+    productUrl = ''
+
+    if productName == '':
+        try:
+            productName = item.find_element(By.CLASS_NAME, 'hQYVAI').text
+        except Exception:
+            pass
+    elif productName == '':
+        try:
+            productName = item.find_element(By.CLASS_NAME, 'sc-hFVsQR').text
+        except Exception:
+            pass
+
+    print(productName)
